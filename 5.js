2b299b51568770be1179a4b765e020a569543116
Kiikurage <y.kikura@gmail.com>
2015-08-24 11:21:13 +0900

    引き算記法を拡張した次数として表現

--- 5.js
+++ 5.js
@@ -1,46 +1,46 @@
 var fs = require('fs');
 
 String.prototype.repeat = function(n) {
-    return (n < 2 ? this : this+this.repeat(n - 1));
+    return (n < 2 ? this : this + this.repeat(n - 1));
 };
 
 if (process.argv.length === 3) {
     main(process.argv[2]);
 } else {
     require('fs').readFile('/dev/stdin', 'utf8', function(err, res) {
-        main(res);
+        main(res.trim());
     });
 }
 
-function main(exp) {
+function main(input) {
+    console.log(encodeRomanNumberExpression(input));
+}
+
+/**
+ * 入力された10進数数字列をローマ数字表記に変換する
+ * @param {string|number} input 入力数字列
+ * @return {number} 変換結果
+ */
+function encodeRomanNumberExpression(input) {
     var res = '',
+        baseList = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        charList = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'],
         d;
 
-    var baseList = [1000, 500, 100, 50, 10, 5, 1],
-        charList = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-
-    exp = Number(exp);
-    while (exp > 0) {
+    input = Number(input);
+    while (input > 0) {
         while (true) {
-            d = Math.floor(exp / baseList[0]);
+            d = Math.floor(input / baseList[0]);
             if (d !== 0) break;
             baseList.shift();
             charList.shift();
         }
 
-        exp -= baseList[0] * d;
+        input -= baseList[0] * d;
         res += charList[0].repeat(d);
         baseList.shift();
         charList.shift();
     }
 
-    res = res
-        .replace(/DCCCC/g, 'CM')
-        .replace(/CCCC/g, 'DM')
-        .replace(/LXXXX/g, 'XC')
-        .replace(/XXXX/g, 'XL')
-        .replace(/VIIII/g, 'IX')
-        .replace(/IIII/g, 'IV')
-
-    console.log(res);
+    return res
 }
