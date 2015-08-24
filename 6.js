c46974fdb5fab2f8b340728ad038f9c7e6721b4a
Kiikurage <y.kikura@gmail.com>
2015-08-24 11:36:48 +0900

    コメント追加。139の扱いなどで、文法に曖昧性が残っている

--- 6.js
+++ 6.js
@@ -8,57 +8,73 @@ if (process.argv.length === 3) {
     main(process.argv[2]);
 } else {
     require('fs').readFile('/dev/stdin', 'utf8', function(err, res) {
-        main(res);
+        main(res).trim();
     });
 }
 
-function main(exp) {
+function main(input) {
+    console.log(encodeRomanNumberExpression(input));
+}
+
+/**
+ * 入力された10進数数字列をローマ数字表記に変換する
+ * @param {string|number} input 入力数字列
+ * @return {number} 変換結果
+ */
+function encodeRomanNumberExpression(input) {
     var res = '',
         cover, d;
 
     var baseList = [1000, 500, 100, 50, 10, 5, 1],
         charList = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
 
-    exp = Number(exp);
-    while (exp > 0) {
-        cover = calcCover(exp);
+    input = Number(input);
+    while (input > 0) {
+        cover = calcCover(input);
 
         if (cover.result) {
-            exp += cover.value;
+            input += cover.value;
+            //毎回、高々1個しか記号は追加されない
+            //（記号を追加した後、本来の隣接する記号βが置かれる）ので、
+            //文法の「記号を１つだけ、」の制約は満たされる。
             res += charList[baseList.indexOf(cover.value)];
         }
 
         while (true) {
-            d = Math.floor(exp / baseList[0]);
+            d = Math.floor(input / baseList[0]);
             if (d !== 0) break;
             baseList.shift();
             charList.shift();
         }
 
-        exp -= baseList[0] * d;
+        input -= baseList[0] * d;
         res += charList[0].repeat(d);
         baseList.shift();
         charList.shift();
     }
 
-    res = res
-        .replace(/DCCCC/g, 'CM')
-        .replace(/CCCC/g, 'DM')
-        .replace(/LXXXX/g, 'XC')
-        .replace(/XXXX/g, 'XL')
-        .replace(/VIIII/g, 'IX')
-        .replace(/IIII/g, 'IV')
-
-    console.log(res);
+    return res;
 }
 
-function calcCover(exp){
+/**
+ * 入力の補数が単純な形で表現できるかを確認する
+ * @param {number} input 入力
+ * @return {Object} 結果
+ * @return {boolean} result 単純な形の補数の場合はtrueを返す
+ * @return {number} value 補数
+ */
+function calcCover(input){
     var ma,
-        exp = exp.toString();
-    if (ma = exp.match(/^(?:(4*9+)|4)/)) {
+        input = input.toString();
+
+    //単純な形で表現できるためには
+    // - 4に対して1を足す
+    // - 9に対して1を足す
+    //が考えられ、それらを網羅した正規表現が次のようになる
+    if (ma = input.match(/(?:(4?9+)|4)/)) {
         return {
             result: true,
-            value: 1*Math.pow(10, exp.length-ma.index-ma[0].length)
+            value: 1*Math.pow(10, input.length-ma.index-ma[0].length)
         }
     } else {
         return {
