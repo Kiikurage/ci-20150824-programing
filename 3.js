4a4c10ed3d3eb219e579c24fa376669b99263d62
Kiikurage <y.kikura@gmail.com>
2015-08-24 10:30:13 +0900

    4まで

--- 3.js
+++ 3.js
@@ -1,26 +1,41 @@
 var fs = require('fs');
 
 if (process.argv.length === 3) {
-    main(process.argv[2], 8);
+    main(process.argv[2]);
 } else {
     require('fs').readFile('/dev/stdin', 'utf8', function(err, res) {
-        main(res, 8);
+        main(res);
     });
 }
 
-function main(exp, base) {
+function main(exp) {
     var i = 0,
         res = 0,
-        w;
+        v, lastV;
     for (i = 0; i < exp.length; i++) {
-        w = exp.charAt[i];
-        res += toNumber(exp.charAt(i));
+        v = toNumber(exp.charAt(i));
+        if (lastV < v) {
+            res -= lastV;
+            res += v-lastV;
+        } else {
+            res += v;
+        }
+        lastV = v;
     }
 
     console.log(res);
 }
 
 function toNumber(w) {
-    var map = 'abcdefgh';
-    return map.indexOf(w);
+    var map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    return map[w];
 }
