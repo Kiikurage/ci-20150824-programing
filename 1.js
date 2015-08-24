e44db4dd5557b738562feea24f48751c568e943e
Kiikurage <y.kikura@gmail.com>
2015-08-24 11:13:53 +0900

    コメントを追加。構造を整理

--- 1.js
+++ 1.js
@@ -1,24 +1,39 @@
 var fs = require('fs');
 
 if (process.argv.length === 3) {
-    main(process.argv[2], 4);
+    main(process.argv[2]);
 } else {
     require('fs').readFile('/dev/stdin', 'utf8', function(err, res) {
-        main(res, 4);
+        main(res.trim());
     });
 }
 
-function main(exp, base) {
+function main(input) {
+    console.log(toDEC(input, 4));
+}
+
+/**
+ * 入力された数字列を10進数に変換する
+ * @param {string} exp 入力数字列
+ * @param {number} baseFrom 基数
+ * @return {number} 変換結果
+ */
+function toDEC(exp, baseFrom) {
     var i = 0,
         res = 0;
     for (i = 0; i < exp.length; i++) {
-        res *= base;
-        res += toNumber(exp.charAt(i));
+        res *= baseFrom;
+        res += toDECNumber(exp.charAt(i));
     }
 
-    console.log(res);
+    return res;
 }
 
-function toNumber(w) {
+/**
+ * 入力された数字を10進数に変換する
+ * @param {string} w 数字
+ * @return {number} 変換結果
+ */
+function toDECNumber(w) {
     return Number(w);
 }
