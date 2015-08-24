53df8595f0754654497bcfdfe7417e49c6fc3a95
Kiikurage <y.kikura@gmail.com>
2015-08-24 11:20:46 +0900

    コメント追加

--- 4.js
+++ 4.js
@@ -4,16 +4,25 @@ if (process.argv.length === 3) {
     main(process.argv[2]);
 } else {
     require('fs').readFile('/dev/stdin', 'utf8', function(err, res) {
-        main(res);
+        main(res.trim());
     });
 }
 
-function main(exp) {
+function main(input) {
+    console.log(decodeRomanNumberExpression(input));
+}
+
+/**
+ * 入力されたローマ数字列を10進数に変換する
+ * @param {string} input 入力数字列
+ * @return {number} 変換結果
+ */
+function decodeRomanNumberExpression(input){
     var i = 0,
         res = 0,
         v, lastV;
-    for (i = 0; i < exp.length; i++) {
-        v = toNumber(exp.charAt(i));
+    for (i = 0; i < input.length; i++) {
+        v = decodeRomanNumber(input.charAt(i));
         if (lastV < v) {
             res -= lastV;
             res += v-lastV;
@@ -23,10 +32,15 @@ function main(exp) {
         lastV = v;
     }
 
-    console.log(res);
+    return res;
 }
 
-function toNumber(w) {
+/**
+ * 入力されたローマ数字1文字を10進数に変換する
+ * @param {string} w ローマ数字
+ * @return {number} 変換結果
+ */
+function decodeRomanNumber(w) {
     var map = {
         'I': 1,
         'V': 5,
